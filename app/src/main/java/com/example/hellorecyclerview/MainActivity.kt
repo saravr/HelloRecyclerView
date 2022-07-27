275c79bbaad1e72b2728437fdd87956889e0ec1e
Sarav Ramaswamy <sarav@yahooinc.com>
2022-07-26 21:30:38 -0700

    Add recyclerview

--- app/src/main/java/com/example/hellorecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/hellorecyclerview/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.hellorecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hellorecyclerview.databinding.ActivityMainBinding
+import com.example.hellorecyclerview.databinding.ItemBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTest.apply {
+            adapter = Adapter()
+        }
+    }
+}
+
+private val names = Array(50) {
+    "Item $it"
+}
+
+class Adapter: RecyclerView.Adapter<Adapter.MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemBinding = ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(names[position])
+    }
+
+    override fun getItemCount() = names.size
+
+    class MyViewHolder(private val itemBinding: ItemBinding): RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(name: String) {
+            itemBinding.tvItemName.text = name
+        }
     }
 }
